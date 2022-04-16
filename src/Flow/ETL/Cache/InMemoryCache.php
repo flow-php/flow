eea2f663346e7cc78b6c629711438923b673e9de
Norbert Orzechowicz <1921950+norberttech@users.noreply.github.com>
2022-04-16 10:38:34 +0200

    Added in memory cache implementation (#223)

---
+++ src/Flow/ETL/Cache/InMemoryCache.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Flow\ETL\Cache;
+
+use Flow\ETL\Cache;
+use Flow\ETL\Rows;
+
+final class InMemoryCache implements Cache
+{
+    /**
+     * @var array<string, array<Rows>>
+     */
+    private array $cache = [];
+
+    public function add(string $id, Rows $rows) : void
+    {
+        if (!\array_key_exists($id, $this->cache)) {
+            $this->cache[$id] = [];
+        }
+
+        $this->cache[$id][] = $rows;
+    }
+
+    public function clear(string $id) : void
+    {
+        if (\array_key_exists($id, $this->cache)) {
+            $this->cache[$id] = [];
+        }
+    }
+
+    public function read(string $id) : \Generator
+    {
+        if (\array_key_exists($id, $this->cache)) {
+            foreach ($this->cache[$id] as $rows) {
+                yield $rows;
+            }
+        }
+    }
+}
