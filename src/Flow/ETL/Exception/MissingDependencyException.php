f6c718950783b9e5cac55d1a3588049a4b56334b
Norbert Orzechowicz <1921950+norberttech@users.noreply.github.com>
2022-05-25 09:12:09 +0200

    Added missing dependency exception (#245)

---
+++ src/Flow/ETL/Exception/MissingDependencyException.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Flow\ETL\Exception;
+
+final class MissingDependencyException extends RuntimeException
+{
+    public function __construct(string $name, string $package, \Exception $previous = null)
+    {
+        parent::__construct(
+            "Missing {$name} dependency, please run 'composer require {$package}'",
+            previous: $previous
+        );
+    }
+}
