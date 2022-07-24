27ee7a709bb315cbf39aaf492c2106421f5c8a29
Norbert Orzechowicz <norbert@orzechowicz.pl>
2022-07-24 16:26:25 +0200

    Added monorepo tools and settings

---
+++ monorepo-builder.php
@@ -0,0 +1,63 @@
+<?php
+
+declare(strict_types=1);
+
+use MonorepoBuilder202207\Symplify\ComposerJsonManipulator\ValueObject\ComposerJsonSection;
+use Symplify\MonorepoBuilder\Config\MBConfig;
+
+return static function (MBConfig $config): void {
+    $config->defaultBranch('1.x');
+    $config->packageDirectories([
+        __DIR__ . '/src/core',
+        __DIR__ . '/src/adapter',
+        __DIR__ . '/src/lib',
+    ]);
+
+    $config->dataToAppend([
+        ComposerJsonSection::SCRIPTS => [
+            "build" => [
+                "@static:analyze",
+                "@test",
+                "@test:mutation"
+            ],
+            "test" => [
+                "tools/phpunit/vendor/bin/phpunit"
+            ],
+            "test:mutation" => [
+                "tools/infection/vendor/bin/infection -j2"
+            ],
+            "static:analyze" => [
+                "tools/psalm/vendor/bin/psalm.phar --output-format=compact",
+                "tools/phpstan/vendor/bin/phpstan analyze -c phpstan.neon",
+                "tools/cs-fixer/vendor/bin/php-cs-fixer fix --dry-run"
+            ],
+            "cs:php:fix" => [
+                "tools/cs-fixer/vendor/bin/php-cs-fixer fix"
+            ],
+            "post-install-cmd" => [
+                "@tools:install"
+            ],
+            "post-update-cmd" => [
+                "@tools:install"
+            ],
+            "tools:install" => [
+                "composer install --working-dir=./tools/cs-fixer",
+                "composer install --working-dir=./tools/infection",
+                "composer install --working-dir=./tools/monorepo",
+                "composer install --working-dir=./tools/phpstan",
+                "composer install --working-dir=./tools/psalm",
+                "composer install --working-dir=./tools/phpunit"
+            ]
+        ]
+    ]);
+
+    $config->dataToRemove([
+        ComposerJsonSection::REQUIRE_DEV => [
+            "phpunit/phpunit" => "*",
+            "infection/infection" => "*",
+            "friendsofphp/php-cs-fixer" => "*",
+            "phpstan/phpstan" => "*",
+            "vimeo/psalm" => "*",
+        ]
+    ]);
+};
