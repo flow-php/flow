32e916e4bbe83995a7fc19d11d982d85eb107611
Norbert Orzechowicz <norbert@orzechowicz.pl>
2022-09-12 21:59:09 +0200

    Added support for Elasticsearch 8 client

--- src/adapter/etl-adapter-elasticsearch/tests/Flow/ETL/Adapter/Elasticsearch/Tests/Integration/TestCase.php
+++ src/adapter/etl-adapter-elasticsearch/tests/Flow/ETL/Adapter/Elasticsearch/Tests/Integration/TestCase.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Flow\ETL\Adapter\Elasticsearch\Tests\Integration;
 
+use Flow\ETL\Adapter\Elasticsearch\Tests\Context\Elasticsearch7Context;
+use Flow\ETL\Adapter\Elasticsearch\Tests\Context\Elasticsearch8Context;
 use Flow\ETL\Adapter\Elasticsearch\Tests\Context\ElasticsearchContext;
 
 abstract class TestCase extends \PHPUnit\Framework\TestCase
@@ -12,6 +14,8 @@ abstract class TestCase extends \PHPUnit\Framework\TestCase
 
     protected function setUp() : void
     {
-        $this->elasticsearchContext = new ElasticsearchContext([\getenv('ELASTICSEARCH_URL')]);
+        $this->elasticsearchContext = (\class_exists("Elasticsearch\Client"))
+            ? new Elasticsearch7Context([\getenv('ELASTICSEARCH_URL')])
+            : new Elasticsearch8Context([\getenv('ELASTICSEARCH_URL')]);
     }
 }
