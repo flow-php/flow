82f681598920980c268347f43595c379f9618137
Norbert Orzechowicz <norbert@orzechowicz.pl>
2023-02-18 09:55:28 +0100

    Fixed monorepo builder config

--- monorepo-builder.php
+++ monorepo-builder.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-use Symplify\ComposerJsonManipulator\ValueObject\ComposerJsonSection;
+use Symplify\MonorepoBuilder\ComposerJsonManipulator\ValueObject\ComposerJsonSection;
 use Symplify\MonorepoBuilder\Config\MBConfig;
 
 return static function (MBConfig $config): void {
