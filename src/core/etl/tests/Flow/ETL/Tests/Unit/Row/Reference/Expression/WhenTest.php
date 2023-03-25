3779c6b61a63c36b8382855ede149cd1e8c93ea0
Norbert Orzechowicz <norbert@orzechowicz.pl>
2023-03-25 15:32:24 +0100

    Added one more unit test for when

--- src/core/etl/tests/Flow/ETL/Tests/Unit/Row/Reference/Expression/WhenTest.php
+++ src/core/etl/tests/Flow/ETL/Tests/Unit/Row/Reference/Expression/WhenTest.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Flow\ETL\Tests\Unit\Row\Reference\Expression;
 
+use function Flow\ETL\DSL\lit;
+use function Flow\ETL\DSL\ref;
 use Flow\ETL\DSL\Entry;
 use Flow\ETL\Row;
 use Flow\ETL\Row\Reference\Expression\Literal;
@@ -12,6 +14,17 @@ use PHPUnit\Framework\TestCase;
 
 final class WhenTest extends TestCase
 {
+    public function test_condition_not_satisfied_without_else() : void
+    {
+        $this->assertSame(
+            1,
+            (new When(
+                ref('id')->equals(lit(2)),
+                new Literal('then')
+            ))->eval(Row::with(Entry::int('id', 1)))
+        );
+    }
+
     public function test_else() : void
     {
         $this->assertSame(
