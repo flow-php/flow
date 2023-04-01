8a9c37259e56b20f21af2007eb23af3a61015385
Norbert Orzechowicz <norbert@orzechowicz.pl>
2023-04-01 12:58:45 +0200

    Allow to change source from which ES extractor is extracting rows

--- src/adapter/etl-adapter-elasticsearch/src/Flow/ETL/Adapter/Elasticsearch/ElasticsearchPHP/HitsIntoRowsTransformer.php
+++ src/adapter/etl-adapter-elasticsearch/src/Flow/ETL/Adapter/Elasticsearch/ElasticsearchPHP/HitsIntoRowsTransformer.php
@@ -12,24 +12,28 @@ use Flow\ETL\Rows;
 use Flow\ETL\Transformer;
 
 /**
- * @implements Transformer<array{entry_factory: EntryFactory}>
+ * @implements Transformer<array{entry_factory: EntryFactory, source: DocumentDataSource}>
  */
 final class HitsIntoRowsTransformer implements Transformer
 {
-    public function __construct(private readonly EntryFactory $entryFactory = new NativeEntryFactory())
-    {
+    public function __construct(
+        private readonly DocumentDataSource $source = DocumentDataSource::source,
+        private readonly EntryFactory $entryFactory = new NativeEntryFactory()
+    ) {
     }
 
     public function __serialize() : array
     {
         return [
             'entry_factory' => $this->entryFactory,
+            'source' => $this->source,
         ];
     }
 
     public function __unserialize(array $data) : void
     {
         $this->entryFactory = $data['entry_factory'];
+        $this->source = $data['source'];
     }
 
     public function transform(Rows $rows, FlowContext $context) : Rows
@@ -42,17 +46,23 @@ final class HitsIntoRowsTransformer implements Transformer
             }
 
             /**
-             * @var array{hits: array<array{_source: array<string, mixed>}>} $hits
+             * @var array{hits: array<array{_source: array<string, mixed>, fields: array<string, mixed>}>} $hits
              */
             $hits = $row->get('hits')->value();
 
             foreach ($hits['hits'] as $hit) {
                 $entries = [];
 
+                $source = match ($this->source) {
+                    DocumentDataSource::source => '_source',
+                    DocumentDataSource::fields => 'fields'
+                };
+
                 /**
+                 * @var string $key
                  * @var mixed $value
                  */
-                foreach ($hit['_source'] as $key => $value) {
+                foreach ($hit[$source] as $key => $value) {
                     $entries[] = $this->entryFactory->create($key, $value);
                 }
 
