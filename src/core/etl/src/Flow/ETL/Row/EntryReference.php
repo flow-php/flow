8b25b8d86543cc9d252ae00e6e0ed78a9a33efc4
Norbert Orzechowicz <norbert@orzechowicz.pl>
2023-04-04 14:16:02 +0200

    Fixed expressions

--- src/core/etl/src/Flow/ETL/Row/EntryReference.php
+++ src/core/etl/src/Flow/ETL/Row/EntryReference.php
@@ -103,7 +103,7 @@ final class EntryReference implements Expression, Reference
 
     public function contains(string|Expression $needle) : self
     {
-        $this->expressions->add(new Contains($this, \is_string($needle) ? self::init($needle) : $needle));
+        $this->expressions = $this->expressions->add(new Contains($this, \is_string($needle) ? self::init($needle) : $needle));
 
         return $this;
     }
@@ -124,7 +124,7 @@ final class EntryReference implements Expression, Reference
 
     public function endsWith(string|Expression $needle) : self
     {
-        $this->expressions->add(new EndsWith($this, \is_string($needle) ? self::init($needle) : $needle));
+        $this->expressions = $this->expressions->add(new EndsWith($this, \is_string($needle) ? self::init($needle) : $needle));
 
         return $this;
     }
@@ -188,7 +188,7 @@ final class EntryReference implements Expression, Reference
 
     public function isIn(string|Expression $haystack) : self
     {
-        $this->expressions->add(new IsIn(\is_string($haystack) ? self::init($haystack) : $haystack, $this));
+        $this->expressions = $this->expressions->add(new IsIn(\is_string($haystack) ? self::init($haystack) : $haystack, $this));
 
         return $this;
     }
