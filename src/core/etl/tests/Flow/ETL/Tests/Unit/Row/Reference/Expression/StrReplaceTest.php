10e7d5ff64caa8d23ce45cbab6473f8830d7d12c
Norbert Orzechowicz <1921950+norberttech@users.noreply.github.com>
2023-04-18 19:51:21 +0200

    Added str replace expression (#401)

---
+++ src/core/etl/tests/Flow/ETL/Tests/Unit/Row/Reference/Expression/StrReplaceTest.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Flow\ETL\Tests\Unit\Row\Reference\Expression;
+
+use function Flow\ETL\DSL\ref;
+use Flow\ETL\DSL\Entry;
+use Flow\ETL\Row;
+use PHPUnit\Framework\TestCase;
+
+final class StrReplaceTest extends TestCase
+{
+    public function test_str_replace_on_non_string_value() : void
+    {
+        $this->assertNull(
+            ref('value')->strReplace('test', '1')->eval(Row::create(Entry::int('value', 1000))),
+        );
+    }
+
+    public function test_str_replace_on_valid_string() : void
+    {
+        $this->assertSame(
+            '1',
+            ref('value')->strReplace('test', '1')->eval(Row::create(Entry::str('value', 'test'))),
+        );
+    }
+}
