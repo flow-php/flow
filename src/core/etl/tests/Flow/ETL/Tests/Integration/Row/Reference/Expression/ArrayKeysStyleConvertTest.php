951bf410ca8f30208c87ef5ec2cccd88a258319d
Joseph Bielawski <stloyd@users.noreply.github.com>
2023-10-08 11:19:25 +0200

    Add integration tests for array related expression (#546)

---
+++ src/core/etl/tests/Flow/ETL/Tests/Integration/Row/Reference/Expression/ArrayKeysStyleConvertTest.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Flow\ETL\Tests\Integration\Row\Reference\Expression;
+
+use function Flow\ETL\DSL\array_keys_style_convert;
+use function Flow\ETL\DSL\ref;
+use Flow\ETL\DSL\From;
+use Flow\ETL\DSL\To;
+use Flow\ETL\Flow;
+use Flow\ETL\Memory\ArrayMemory;
+use PHPUnit\Framework\TestCase;
+
+final class ArrayKeysStyleConvertTest extends TestCase
+{
+    public function test_array_keys_style_convert() : void
+    {
+        (new Flow())
+            ->read(
+                From::array(
+                    [
+                        ['id' => 1, 'array' => ['camelCased' => 1, 'snake_cased' => 2, 'space word' => 3]],
+                    ]
+                )
+            )
+            ->withEntry('row', ref('row')->unpack())
+            ->renameAll('row.', '')
+            ->withEntry('array', array_keys_style_convert(ref('array'), 'kebab'))
+            ->drop('row')
+            ->write(To::memory($memory = new ArrayMemory()))
+            ->run();
+
+        $this->assertSame(
+            [
+                ['id' => 1, 'array' => ['camel-cased' => 1, 'snake-cased' => 2, 'space-word' => 3]],
+            ],
+            $memory->data
+        );
+    }
+}
---
+++ src/core/etl/tests/Flow/ETL/Tests/Integration/Row/Reference/Expression/ArraySortTest.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Flow\ETL\Tests\Integration\Row\Reference\Expression;
+
+use function Flow\ETL\DSL\ref;
+use Flow\ETL\DSL\From;
+use Flow\ETL\DSL\To;
+use Flow\ETL\Flow;
+use Flow\ETL\Memory\ArrayMemory;
+use PHPUnit\Framework\TestCase;
+
+final class ArraySortTest extends TestCase
+{
+    public function test_array_sort() : void
+    {
+        (new Flow())
+            ->read(
+                From::array(
+                    [
+                        ['id' => 1, 'array' => ['a' => 1, 'c' => 3, 'b' => 2]],
+                        ['id' => 2],
+                    ]
+                )
+            )
+            ->withEntry('row', ref('row')->unpack())
+            ->renameAll('row.', '')
+            ->drop('row')
+            ->withEntry('array', ref('array')->arraySort('ksort'))
+            ->write(To::memory($memory = new ArrayMemory()))
+            ->run();
+
+        $this->assertSame(
+            [
+                ['id' => 1, 'array' => ['a' => 1, 'b' => 2, 'c' => 3]],
+                ['id' => 2, 'array' => null],
+            ],
+            $memory->data
+        );
+    }
+}
