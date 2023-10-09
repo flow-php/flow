72be677fcfd2439012278d0a05b82659564ce15e
Joseph Bielawski <stloyd@users.noreply.github.com>
2023-10-09 13:28:00 +0200

    Add integration tests for `jsonDecode` & `jsonEncode` expressions (#550)

---
+++ src/core/etl/tests/Flow/ETL/Tests/Integration/Row/Reference/Expression/JsonDecodeTest.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Flow\ETL\Tests\Integration\Row\Reference\Expression;
+
+use function Flow\ETL\DSL\lit;
+use function Flow\ETL\DSL\ref;
+use Flow\ETL\DSL\From;
+use Flow\ETL\DSL\To;
+use Flow\ETL\Flow;
+use Flow\ETL\Memory\ArrayMemory;
+use PHPUnit\Framework\TestCase;
+
+final class JsonDecodeTest extends TestCase
+{
+    public function test_add_json_string_into_existing_reference() : void
+    {
+        (new Flow())
+            ->read(
+                From::array(
+                    [['id' => 1, 'array' => ['a' => 1, 'b' => 2, 'c' => 3]]],
+                )
+            )
+            ->withEntry('row', ref('row')->unpack())
+            ->renameAll('row.', '')
+            ->drop('row')
+            ->withEntry('json', lit('{"d": 4}'))
+            ->withEntry('array', ref('array')->arrayMerge(ref('json')->jsonDecode()))
+            ->drop('json')
+            ->write(To::memory($memory = new ArrayMemory()))
+            ->run();
+
+        $this->assertSame(
+            [
+                ['id' => 1, 'array' => ['a' => 1, 'b' => 2, 'c' => 3, 'd' => 4]],
+            ],
+            $memory->data
+        );
+    }
+}
---
+++ src/core/etl/tests/Flow/ETL/Tests/Integration/Row/Reference/Expression/JsonEncodeTest.php
@@ -0,0 +1,66 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Flow\ETL\Tests\Integration\Row\Reference\Expression;
+
+use function Flow\ETL\DSL\lit;
+use function Flow\ETL\DSL\ref;
+use Flow\ETL\DSL\From;
+use Flow\ETL\DSL\To;
+use Flow\ETL\Flow;
+use Flow\ETL\Memory\ArrayMemory;
+use PHPUnit\Framework\TestCase;
+
+final class JsonEncodeTest extends TestCase
+{
+    public function test_adding_json_as_object_from_string_entry() : void
+    {
+        (new Flow())
+            ->read(
+                From::array([['id' => 1]])
+            )
+            ->withEntry('row', ref('row')->unpack())
+            ->renameAll('row.', '')
+            ->drop('row')
+            ->withEntry('json', lit(['id' => 1, 'name' => 'test']))
+            ->withEntry('json', ref('json')->jsonEncode(\JSON_FORCE_OBJECT))
+            ->write(To::memory($memory = new ArrayMemory()))
+            ->run();
+
+        $this->assertSame(
+            [
+                [
+                    'id' => 1,
+                    'json' => '{"id":1,"name":"test"}',
+                ],
+            ],
+            $memory->data
+        );
+    }
+
+    public function test_adding_json_from_string_entry() : void
+    {
+        (new Flow())
+            ->read(
+                From::array([['id' => 1]])
+            )
+            ->withEntry('row', ref('row')->unpack())
+            ->renameAll('row.', '')
+            ->drop('row')
+            ->withEntry('json', lit([1, 2, 3]))
+            ->withEntry('json', ref('json')->jsonEncode())
+            ->write(To::memory($memory = new ArrayMemory()))
+            ->run();
+
+        $this->assertSame(
+            [
+                [
+                    'id' => 1,
+                    'json' => '[1,2,3]',
+                ],
+            ],
+            $memory->data
+        );
+    }
+}
