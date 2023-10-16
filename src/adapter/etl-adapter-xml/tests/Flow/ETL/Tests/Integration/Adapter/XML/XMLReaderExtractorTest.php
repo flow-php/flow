68916abdfb0cfeab5ecd4b428530ae8e9bf1277d
Joseph Bielawski <stloyd@users.noreply.github.com>
2023-10-16 10:36:38 +0200

    Add PHPBench tool and first benchmark example (#581)

--- src/adapter/etl-adapter-xml/tests/Flow/ETL/Tests/Integration/Adapter/XML/XMLReaderExtractorTest.php
+++ src/adapter/etl-adapter-xml/tests/Flow/ETL/Tests/Integration/Adapter/XML/XMLReaderExtractorTest.php
@@ -16,12 +16,12 @@ final class XMLReaderExtractorTest extends TestCase
     public function test_reading_xml() : void
     {
         $xml = new \DOMDocument();
-        $xml->load(__DIR__ . '/xml/simple_items.xml');
+        $xml->load(__DIR__ . '/../../../Fixtures/simple_items.xml');
 
         $this->assertEquals(
             (new Rows(Row::create(Entry::xml('node', $xml)))),
             (new Flow())
-                ->read(XML::from(__DIR__ . '/xml/simple_items.xml'))
+                ->read(XML::from(__DIR__ . '/../../../Fixtures/simple_items.xml'))
                 ->fetch()
         );
     }
@@ -37,7 +37,7 @@ final class XMLReaderExtractorTest extends TestCase
                 Row::create(Entry::xml('node', '<item item_attribute_01="5"><id id_attribute_01="5">5</id></item>')),
             ),
             (new Flow())
-                ->read(XML::from(__DIR__ . '/xml/simple_items_flat.xml', 'root/items/item'))
+                ->read(XML::from(__DIR__ . '/../../../Fixtures/simple_items_flat.xml', 'root/items/item'))
                 ->fetch()
         );
     }
@@ -69,7 +69,7 @@ XML);
         $this->assertEquals(
             new Rows(Row::create(Entry::xml('node', $xml))),
             (new Flow())
-                ->read(XML::from(__DIR__ . '/xml/simple_items.xml', 'root/items'))
+                ->read(XML::from(__DIR__ . '/../../../Fixtures/simple_items.xml', 'root/items'))
                 ->fetch()
         );
     }
