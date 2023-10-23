0ba756fa475d1e23a39f7f5b9ae3ef0a1d3ee166
Norbert Orzechowicz <contact@norbert.tech>
2023-10-23 15:07:01 +0200

    Removed unused supression

--- src/adapter/etl-adapter-google-sheet/src/Flow/ETL/Adapter/GoogleSheet/GoogleSheetExtractor.php
+++ src/adapter/etl-adapter-google-sheet/src/Flow/ETL/Adapter/GoogleSheet/GoogleSheetExtractor.php
@@ -58,7 +58,6 @@ final class GoogleSheetExtractor implements Extractor
                             \array_push(
                                 $rowData,
                                 ...\array_map(
-                                    /** @psalm-suppress UnusedClosureParam */
                                     static fn (int $i) => null,
                                     \range(1, \count($headers) - \count($rowData))
                                 )
