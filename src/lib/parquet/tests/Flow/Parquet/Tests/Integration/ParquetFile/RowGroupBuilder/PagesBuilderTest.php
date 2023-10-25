6e62772dedcbe574055213d1bf1d997ba6a498fb
Norbert Orzechowicz <1921950+norberttech@users.noreply.github.com>
2023-10-25 23:31:54 +0200

    Move more logical types to dictionary encoding (#648)

--- src/lib/parquet/tests/Flow/Parquet/Tests/Integration/ParquetFile/RowGroupBuilder/PagesBuilderTest.php
+++ src/lib/parquet/tests/Flow/Parquet/Tests/Integration/ParquetFile/RowGroupBuilder/PagesBuilderTest.php
@@ -16,6 +16,49 @@ use PHPUnit\Framework\TestCase;
 
 final class PagesBuilderTest extends TestCase
 {
+    public function test_building_pages_for_enum_columns() : void
+    {
+        $column = FlatColumn::enum('enum');
+        $enum = [
+            0 => 'RED',
+            1 => 'GREEN',
+            2 => 'BLUE',
+        ];
+
+        $values = \array_map(static fn ($i) => $enum[\random_int(0, 2)], \range(0, 99));
+
+        $pages = (new PagesBuilder(DataConverter::initialize(new Options())))->build($column, $values);
+
+        $this->assertEquals(
+            new PageHeader(
+                Type::DICTIONARY_PAGE,
+                \strlen($pages->dictionaryPageContainer()->pageBuffer),
+                \strlen($pages->dictionaryPageContainer()->pageBuffer),
+                null,
+                null,
+                new DictionaryPageHeader(
+                    Encodings::PLAIN,
+                    $pages->valuesCount(),
+                )
+            ),
+            $pages->dictionaryPageContainer()->pageHeader
+        );
+        $this->assertEquals(
+            new PageHeader(
+                Type::DATA_PAGE,
+                \strlen($pages->dataPageContainers()[0]->pageBuffer),
+                \strlen($pages->dataPageContainers()[0]->pageBuffer),
+                new DataPageHeader(
+                    Encodings::PLAIN_DICTIONARY,
+                    \count($values),
+                ),
+                null,
+                null
+            ),
+            $pages->dataPageContainers()[0]->pageHeader
+        );
+    }
+
     public function test_building_pages_for_integer_column() : void
     {
         $column = FlatColumn::int32('int32');
@@ -40,11 +83,87 @@ final class PagesBuilderTest extends TestCase
         );
     }
 
+    public function test_building_pages_for_json_columns() : void
+    {
+        $column = FlatColumn::json('json');
+        $faker = Factory::create();
+        $values = \array_map(static fn ($i) => \json_encode(['id' => $faker->uuid], JSON_THROW_ON_ERROR), \range(0, 99));
+
+        $pages = (new PagesBuilder(DataConverter::initialize(new Options())))->build($column, $values);
+
+        $this->assertEquals(
+            new PageHeader(
+                Type::DICTIONARY_PAGE,
+                \strlen($pages->dictionaryPageContainer()->pageBuffer),
+                \strlen($pages->dictionaryPageContainer()->pageBuffer),
+                null,
+                null,
+                new DictionaryPageHeader(
+                    Encodings::PLAIN,
+                    $pages->valuesCount(),
+                )
+            ),
+            $pages->dictionaryPageContainer()->pageHeader
+        );
+        $this->assertEquals(
+            new PageHeader(
+                Type::DATA_PAGE,
+                \strlen($pages->dataPageContainers()[0]->pageBuffer),
+                \strlen($pages->dataPageContainers()[0]->pageBuffer),
+                new DataPageHeader(
+                    Encodings::PLAIN_DICTIONARY,
+                    \count($values),
+                ),
+                null,
+                null
+            ),
+            $pages->dataPageContainers()[0]->pageHeader
+        );
+    }
+
     public function test_building_pages_for_string_columns() : void
     {
         $column = FlatColumn::string('string');
         $faker = Factory::create();
         $values = \array_map(static fn ($i) => $faker->text(10), \range(0, 99));
+
+        $pages = (new PagesBuilder(DataConverter::initialize(new Options())))->build($column, $values);
+
+        $this->assertEquals(
+            new PageHeader(
+                Type::DICTIONARY_PAGE,
+                \strlen($pages->dictionaryPageContainer()->pageBuffer),
+                \strlen($pages->dictionaryPageContainer()->pageBuffer),
+                null,
+                null,
+                new DictionaryPageHeader(
+                    Encodings::PLAIN,
+                    $pages->valuesCount(),
+                )
+            ),
+            $pages->dictionaryPageContainer()->pageHeader
+        );
+        $this->assertEquals(
+            new PageHeader(
+                Type::DATA_PAGE,
+                \strlen($pages->dataPageContainers()[0]->pageBuffer),
+                \strlen($pages->dataPageContainers()[0]->pageBuffer),
+                new DataPageHeader(
+                    Encodings::PLAIN_DICTIONARY,
+                    \count($values),
+                ),
+                null,
+                null
+            ),
+            $pages->dataPageContainers()[0]->pageHeader
+        );
+    }
+
+    public function test_building_pages_for_uuid_columns() : void
+    {
+        $column = FlatColumn::string('uuid');
+        $faker = Factory::create();
+        $values = \array_map(static fn ($i) => $faker->uuid, \range(0, 99));
 
         $pages = (new PagesBuilder(DataConverter::initialize(new Options())))->build($column, $values);
 
