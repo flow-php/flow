bf4dbeeec3323a8fce410d5d51eab3437cefc49a
Joseph Bielawski <stloyd@gmail.com>
2023-10-29 17:27:59 +0100

    Add benchmark for `Rows::chunk()`

---
+++ src/core/etl/tests/Flow/ETL/Tests/Benchmark/RowsBench.php
@@ -0,0 +1,34 @@
+<?php declare(strict_types=1);
+
+use Flow\ETL\Rows;
+use PhpBench\Attributes\Groups;
+use PhpBench\Attributes\Iterations;
+use PhpBench\Attributes\Revs;
+
+#[Iterations(5)]
+#[Groups(['rows'])]
+final class RowsBench
+{
+    private Rows $rows;
+
+    public function __construct()
+    {
+        $this->rows = Rows::fromArray(
+            \array_merge(...\array_map(static fn () : array => [
+                ['id' => 1, 'random' => false, 'text' => null, 'from' => 666],
+                ['id' => 2, 'random' => true, 'text' => null, 'from' => 666],
+                ['id' => 3, 'random' => false, 'text' => null, 'from' => 666],
+                ['id' => 4, 'random' => true, 'text' => null, 'from' => 666],
+                ['id' => 5, 'random' => false, 'text' => null, 'from' => 666],
+            ], \range(0, 10_000)))
+        );
+    }
+
+    #[Revs(5)]
+    public function bench_chunk_10() : void
+    {
+        foreach ($this->rows->chunks(10) as $chunk) {
+
+        }
+    }
+}
