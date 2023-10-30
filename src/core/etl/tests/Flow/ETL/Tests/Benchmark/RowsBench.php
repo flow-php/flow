26d2f7608904ee15480703a041f21292061a6197
Joseph Bielawski <stloyd@gmail.com>
2023-10-30 19:39:43 +0100

    Rework benchmarks to be more reliable

--- src/core/etl/tests/Flow/ETL/Tests/Benchmark/RowsBench.php
+++ src/core/etl/tests/Flow/ETL/Tests/Benchmark/RowsBench.php
@@ -4,11 +4,12 @@ use function Flow\ETL\DSL\ref;
 use Flow\ETL\Row;
 use Flow\ETL\Row\Entry\StringEntry;
 use Flow\ETL\Rows;
+use PhpBench\Attributes\BeforeMethods;
 use PhpBench\Attributes\Groups;
-use PhpBench\Attributes\Iterations;
 use PhpBench\Attributes\Revs;
 
-#[Iterations(5)]
+#[BeforeMethods('setUp')]
+#[Revs(2)]
 #[Groups(['building_blocks'])]
 final class RowsBench
 {
@@ -16,7 +17,7 @@ final class RowsBench
 
     private Rows $rows;
 
-    public function __construct()
+    public function setUp() : void
     {
         $this->rows = Rows::fromArray(
             \array_merge(...\array_map(static fn () : array => [
@@ -39,7 +40,6 @@ final class RowsBench
         );
     }
 
-    #[Revs(5)]
     public function bench_chunk_10_on_10k() : void
     {
         foreach ($this->rows->chunks(10) as $chunk) {
@@ -47,31 +47,26 @@ final class RowsBench
         }
     }
 
-    #[Revs(5)]
     public function bench_diff_left_1k_on_10k() : void
     {
         $this->rows->diffLeft($this->reducedRows);
     }
 
-    #[Revs(5)]
     public function bench_diff_right_1k_on_10k() : void
     {
         $this->rows->diffRight($this->reducedRows);
     }
 
-    #[Revs(5)]
     public function bench_drop_1k_on_10k() : void
     {
         $this->rows->drop(1000);
     }
 
-    #[Revs(5)]
     public function bench_drop_right_1k_on_10k() : void
     {
         $this->rows->dropRight(1000);
     }
 
-    #[Revs(5)]
     public function bench_entries_on_10k() : void
     {
         foreach ($this->rows->entries() as $entries) {
@@ -79,31 +74,28 @@ final class RowsBench
         }
     }
 
-    #[Revs(5)]
     public function bench_filter_on_10k() : void
     {
         $this->rows->filter(fn (Row $row) : bool => $row->valueOf('random') === true);
     }
 
-    #[Revs(5)]
     public function bench_find_on_10k() : void
     {
         $this->rows->find(fn (Row $row) : bool => $row->valueOf('random') === true);
     }
 
-    #[Revs(5)]
+    #[Revs(10)]
     public function bench_find_one_on_10k() : void
     {
         $this->rows->findOne(fn (Row $row) : bool => $row->valueOf('random') === true);
     }
 
-    #[Revs(5)]
+    #[Revs(10)]
     public function bench_first_on_10k() : void
     {
         $this->rows->first();
     }
 
-    #[Revs(5)]
     public function bench_flat_map_on_1k() : void
     {
         $this->reducedRows->flatMap(fn (Row $row) : array => [
@@ -112,73 +104,63 @@ final class RowsBench
         ]);
     }
 
-    #[Revs(5)]
     public function bench_map_on_10k() : void
     {
         $this->rows->map(fn (Row $row) : Row => $row->rename('random', 'whatever'));
     }
 
-    #[Revs(5)]
     public function bench_merge_1k_on_10k() : void
     {
         $this->rows->merge($this->reducedRows);
     }
 
-    #[Revs(5)]
     public function bench_partition_by_on_10k() : void
     {
         $this->rows->partitionBy(ref('from'));
     }
 
-    #[Revs(5)]
     public function bench_remove_on_10k() : void
     {
         $this->rows->remove(1001);
     }
 
-    #[Revs(5)]
     public function bench_sort_asc_on_1k() : void
     {
         $this->reducedRows->sortAscending(ref('random'));
     }
 
-    #[Revs(5)]
     public function bench_sort_by_on_1k() : void
     {
         $this->reducedRows->sortBy(ref('random'));
     }
 
-    #[Revs(5)]
     public function bench_sort_desc_on_1k() : void
     {
         $this->reducedRows->sortDescending(ref('random'));
     }
 
-    #[Revs(5)]
     public function bench_sort_entries_on_1k() : void
     {
         $this->reducedRows->sortEntries();
     }
 
-    #[Revs(5)]
     public function bench_sort_on_1k() : void
     {
         $this->reducedRows->sort(fn (Row $row, Row $nextRow) : int => $row->valueOf('random') <=> $nextRow->valueOf('random'));
     }
 
-    #[Revs(5)]
+    #[Revs(10)]
     public function bench_take_1k_on_10k() : void
     {
         $this->rows->take(1000);
     }
 
-    #[Revs(5)]
+    #[Revs(10)]
     public function bench_take_right_1k_on_10k() : void
     {
         $this->rows->takeRight(1000);
     }
 
-    #[Revs(5)]
     public function bench_unique_on_1k() : void
     {
         $this->rows->unique();
