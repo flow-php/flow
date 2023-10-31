c3ae88e887dbf14ed58217804b3889198dcf9acd
Joseph Bielawski <stloyd@gmail.com>
2023-10-31 11:33:08 +0100

    Improve adapters benchmark stability

--- src/adapter/etl-adapter-parquet/tests/Flow/ETL/Adapter/Parquet/Tests/Benchmark/ParquetLoaderBench.php
+++ src/adapter/etl-adapter-parquet/tests/Flow/ETL/Adapter/Parquet/Tests/Benchmark/ParquetLoaderBench.php
@@ -7,21 +7,20 @@ use Flow\ETL\DSL\Parquet;
 use Flow\ETL\FlowContext;
 use Flow\ETL\Rows;
 use PhpBench\Attributes\Groups;
-use PhpBench\Attributes\Iterations;
-use PhpBench\Attributes\Revs;
 
-#[Iterations(3)]
 #[Groups(['loader'])]
 final class ParquetLoaderBench
 {
-    private FlowContext $context;
+    private readonly FlowContext $context;
+
+    private readonly string $outputPath;
 
     private Rows $rows;
 
     public function __construct()
     {
         $this->context = new FlowContext(Config::default());
-
+        $this->outputPath = \tempnam(\sys_get_temp_dir(), 'etl_parquet_loader_bench') . '.parquet';
         $this->rows = new Rows();
 
         foreach (Parquet::from(__DIR__ . '/../Fixtures/orders_flow.parquet')->extract($this->context) as $rows) {
@@ -29,14 +28,17 @@ final class ParquetLoaderBench
         }
     }
 
-    #[Revs(5)]
+    public function __destruct()
+    {
+        if (!\file_exists($this->outputPath)) {
+            throw new \RuntimeException("Benchmark failed, \"{$this->outputPath}\" doesn't exist");
+        }
+
+        \unlink($this->outputPath);
+    }
+
     public function bench_load_10k() : void
     {
-        Parquet::to($outputPath = \tempnam(\sys_get_temp_dir(), 'etl_parquet_loader_bench') . '.parquet')->load($this->rows, $this->context);
-
-        if (!\file_exists($outputPath)) {
-            throw new \RuntimeException("Benchmark failed, \"{$outputPath}\" doesn't exist");
-        }
-        \unlink($outputPath);
+        Parquet::to($this->outputPath)->load($this->rows, $this->context);
     }
 }
