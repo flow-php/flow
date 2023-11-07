d65a6001a32af857ab47ccd332ddb28a5f944501
Joseph Bielawski <stloyd@gmail.com>
2023-11-07 18:00:51 +0100

    Move PHAR runtime into `bin`

--- src/core/etl/src/Flow/ETL/FlowVersion.php
+++ src/core/etl/src/Flow/ETL/FlowVersion.php
@@ -2,8 +2,16 @@
 
 declare(strict_types=1);
 
+namespace Flow\ETL;
+
+/**
+ * @interal
+ */
 final class FlowVersion
 {
+    /**
+     * Content of this method is replaced by Box library during PHAR generation.
+     */
     public static function getVersion() : string
     {
         return '@git_version@';
