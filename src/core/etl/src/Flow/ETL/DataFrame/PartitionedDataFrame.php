9c1983b993aaaf08f6f7d564eae916a1bc3dfa41
Norbert Orzechowicz <1921950+norberttech@users.noreply.github.com>
2024-01-02 17:28:03 +0100

    Make sure that partitions order is not changed during partitioning (#902)

--- src/core/etl/src/Flow/ETL/DataFrame/PartitionedDataFrame.php
+++ src/core/etl/src/Flow/ETL/DataFrame/PartitionedDataFrame.php
@@ -57,6 +57,14 @@ final class PartitionedDataFrame
         return $this;
     }
 
+    /**
+     * @param null|callable(Rows $rows): void $callback
+     */
+    public function run(?callable $callback = null) : void
+    {
+        $this->df->run($callback);
+    }
+
     public function saveMode(SaveMode $mode) : self
     {
         $this->df->mode($mode);
