ae51c147ad5f6b3b25ad2644388246de8c3a7561
Lukasz Owsiak <owsiakl@outlook.com>
2024-02-01 17:42:57 +0200

    Added html for a landing page

--- src/web/landing/public/index.php
+++ src/web/landing/public/index.php
@@ -1,8 +1,8 @@
-<?php
+<?php declare(strict_types=1);
 
 use Flow\Website\Kernel;
 
-require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+require_once \dirname(__DIR__) . '/vendor/autoload_runtime.php';
 
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
