6b4f0d9302884ce4f40239dd81d207c4b462ba91
Łukasz Owsiak <owsiakl@outlook.com>
2024-02-01 20:18:45 +0200

    Added html for a landing page (#945)

--- src/web/landing/tailwind.config.js
+++ src/web/landing/tailwind.config.js
@@ -5,7 +5,27 @@ module.exports = {
     "./templates/**/*.html.twig",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        black: "#1b1925",
+        gray: "#323232",
+        orange: {
+          100: "#ff5547",
+          200: "#DB4B3D",
+          300: "#B33D32",
+        },
+        blue: {
+          100: "#806dfe",
+          200: "#5945d8",
+          300: "#4026ac",
+        },
+        brown: {
+          100: "#e1761a",
+          200: "#ad6126",
+          300: "#8c4716",
+        }
+      },
+    },
   },
   plugins: [],
 }
