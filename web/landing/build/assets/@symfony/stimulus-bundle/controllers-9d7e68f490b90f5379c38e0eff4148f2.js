debc7340b03a29c2f60d603c155d5970d1302897
Norbert Orzechowicz <contact@norbert.tech>
2024-02-04 15:23:50 +0100

    Removed build foler from repo:

--- web/landing/build/assets/@symfony/stimulus-bundle/controllers-9d7e68f490b90f5379c38e0eff4148f2.js
+++
@@ -1,4 +0,0 @@
-import controller_0 from "../../controllers/syntax_highlight_controller.js";
-export const eagerControllers = {"syntax-highlight": controller_0};
-export const lazyControllers = {};
-export const isApplicationDebug = true;
--- web/landing/build/assets/app-b7fed2530f713e7e81b2af93588bb87a.js
+++
@@ -1,4 +0,0 @@
-import '@fontsource-variable/cabin/index.min.css';
-import 'highlight.js/styles/github-dark.min.css';
-import '@oddbird/popover-polyfill';
-import './bootstrap.js';
