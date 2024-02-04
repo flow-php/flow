75559d1535d124cca17851634c69a69f6c93ea03
Łukasz Owsiak <owsiakl@outlook.com>
2024-02-04 14:27:23 +0200

    Added separate routes for topics and examples to a landing page (#960)

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -13,7 +13,8 @@ $finder = Finder::create()
         __DIR__ . '/src/adapter/**/tests',
         __DIR__ . '/src/lib/**/src',
         __DIR__ . '/src/lib/**/tests',
-        __DIR__ . '/src/web/**/src',
+        __DIR__ . '/web/**/src',
+        __DIR__ . '/web/**/tests',
         __DIR__ . '/examples',
     ]);
 
---
+++ web/landing/src/Flow/Website/Controller/DefaultController.php
@@ -0,0 +1,75 @@
+<?php declare(strict_types=1);
+
+namespace Flow\Website\Controller;
+
+use Flow\Website\Service\Examples;
+use Flow\Website\Service\Github;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class DefaultController extends AbstractController
+{
+    public function __construct(
+        private readonly Github $github,
+        private readonly Examples $examples,
+    ) {
+    }
+
+    #[Route('/{topic}/{example}', name: 'example')]
+    public function example(string $topic, string $example) : Response
+    {
+        $topics = $this->examples->topics();
+        $currentTopic = $topic;
+
+        $examples = $this->examples->examples($currentTopic);
+        $currentExample = $example;
+
+        return $this->render('main/index.html.twig', [
+            'contributors' => $this->github->contributors(),
+            'topics' => $topics,
+            'examples' => $examples,
+            'currentTopic' => $topic,
+            'currentExample' => $example,
+            'code' => $this->examples->code($currentTopic, $currentExample),
+        ]);
+    }
+
+    #[Route('/', name: 'main')]
+    public function main() : Response
+    {
+        $topics = $this->examples->topics();
+        $currentTopic = \current($topics);
+
+        $examples = $this->examples->examples($currentTopic);
+        $currentExample = \current($examples);
+
+        return $this->render('main/index.html.twig', [
+            'contributors' => $this->github->contributors(),
+            'topics' => $topics,
+            'examples' => $examples,
+            'currentTopic' => $currentTopic,
+            'currentExample' => $currentExample,
+            'code' => $this->examples->code($currentTopic, $currentExample),
+        ]);
+    }
+
+    #[Route('/{topic}', name: 'topic')]
+    public function topic(string $topic) : Response
+    {
+        $topics = $this->examples->topics();
+        $currentTopic = $topic;
+
+        $examples = $this->examples->examples($currentTopic);
+        $currentExample = \current($examples);
+
+        return $this->render('main/index.html.twig', [
+            'contributors' => $this->github->contributors(),
+            'topics' => $topics,
+            'examples' => $examples,
+            'currentTopic' => $currentTopic,
+            'currentExample' => $currentExample,
+            'code' => $this->examples->code($currentTopic, $currentExample),
+        ]);
+    }
+}
