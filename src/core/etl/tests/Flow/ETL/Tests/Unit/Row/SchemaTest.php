ce6acd653bc5a00c396da1da573c9f05cd0689f2
Norbert Orzechowicz <1921950+norberttech@users.noreply.github.com>
2024-02-15 11:21:05 +0100

    Improved schema manipulation (#991)

--- src/core/etl/tests/Flow/ETL/Tests/Unit/Row/SchemaTest.php
+++ src/core/etl/tests/Flow/ETL/Tests/Unit/Row/SchemaTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Flow\ETL\Tests\Unit\Row;
 
+use function Flow\ETL\DSL\bool_schema;
 use function Flow\ETL\DSL\int_schema;
 use function Flow\ETL\DSL\json_schema;
 use function Flow\ETL\DSL\list_schema;
@@ -21,15 +22,44 @@ use function Flow\ETL\DSL\type_string;
 use function Flow\ETL\DSL\type_structure;
 use function Flow\ETL\DSL\uuid_schema;
 use Flow\ETL\Exception\InvalidArgumentException;
+use Flow\ETL\Exception\SchemaDefinitionNotFoundException;
+use Flow\ETL\Exception\SchemaDefinitionNotUniqueException;
 use Flow\ETL\Row\EntryReference;
 use Flow\ETL\Row\Schema;
 use PHPUnit\Framework\TestCase;
 
 final class SchemaTest extends TestCase
 {
+    public function test_adding_duplicated_definitions() : void
+    {
+        $this->expectException(SchemaDefinitionNotUniqueException::class);
+        schema(
+            int_schema('id'),
+            str_schema('str', true),
+        )->add(int_schema('str'));
+    }
+
+    public function test_adding_new_definitions() : void
+    {
+        $schema = schema(
+            int_schema('id'),
+            str_schema('str', true),
+        )->add(int_schema('number'), bool_schema('bool'));
+
+        $this->assertEquals(
+            schema(
+                int_schema('id'),
+                str_schema('str', true),
+                int_schema('number'),
+                bool_schema('bool'),
+            ),
+            $schema
+        );
+    }
+
     public function test_allowing_only_unique_definitions() : void
     {
-        $this->expectException(InvalidArgumentException::class);
+        $this->expectException(SchemaDefinitionNotUniqueException::class);
 
         new Schema(
             Schema\Definition::integer('id'),
@@ -69,6 +99,21 @@ final class SchemaTest extends TestCase
         $this->expectExceptionMessage('Schema definition "type" must be an array, got: "test"');
 
         schema_from_json('[{"ref": "id", "type": "test", "metadata": []}]');
+    }
+
+    public function test_graceful_remove_non_existing_definition() : void
+    {
+
+        $this->assertEquals(
+            schema(
+                int_schema('id'),
+                str_schema('name'),
+            ),
+            schema(
+                int_schema('id'),
+                str_schema('name'),
+            )->gracefulRemove('not-existing')
+        );
     }
 
     public function test_making_whole_schema_nullable() : void
@@ -159,18 +204,87 @@ final class SchemaTest extends TestCase
         );
     }
 
+    public function test_remove_non_existing_definition() : void
+    {
+        $this->expectException(SchemaDefinitionNotFoundException::class);
+
+        schema(
+            int_schema('id'),
+            str_schema('name'),
+        )->remove('not-existing');
+    }
+
     public function test_removing_elements_from_schema() : void
     {
-        $schema = new Schema(
-            Schema\Definition::integer('id'),
-            Schema\Definition::string('name'),
+        $this->assertEquals(
+            schema(
+                int_schema('id'),
+            ),
+            schema(
+                int_schema('id'),
+                str_schema('name'),
+            )->without('name')
+        );
+        $this->assertEquals(
+            schema(
+                int_schema('id'),
+            ),
+            schema(
+                int_schema('id'),
+                str_schema('name'),
+            )->remove('name')
+        );
+    }
+
+    public function test_rename() : void
+    {
+        $schema = schema(
+            int_schema('id'),
+            str_schema('name'),
         );
 
         $this->assertEquals(
-            new Schema(
-                Schema\Definition::integer('id'),
+            schema(
+                int_schema('id'),
+                str_schema('new_name'),
             ),
-            $schema->without('name', 'tags')
+            $schema->rename('name', 'new_name')
+        );
+    }
+
+    public function test_rename_non_existing() : void
+    {
+        $this->expectException(SchemaDefinitionNotFoundException::class);
+
+        schema(
+            int_schema('id'),
+            str_schema('name'),
+        )->rename('not-existing', 'new_name');
+    }
+
+    public function test_replace_non_existing_reference() : void
+    {
+        $this->expectException(SchemaDefinitionNotFoundException::class);
+
+        schema(
+            int_schema('id'),
+            str_schema('str', true),
+        )->replace('not-existing', int_schema('number'));
+    }
+
+    public function test_replace_reference() : void
+    {
+        $schema = schema(
+            int_schema('id'),
+            str_schema('str', true),
+        )->replace('str', int_schema('number'));
+
+        $this->assertEquals(
+            schema(
+                int_schema('id'),
+                int_schema('number'),
+            ),
+            $schema
         );
     }
 
