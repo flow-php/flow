9b0b6b6c40e2654d1c5ffc280a9838d963cf0c48
Joseph Bielawski <stloyd@gmail.com>
2024-05-25 18:00:08 +0200

    Fix fragile test comparing console output

--- src/lib/parquet-viewer/tests/Flow/ParquetViewer/Tests/Integration/ReadMetadataTest.php
+++ src/lib/parquet-viewer/tests/Flow/ParquetViewer/Tests/Integration/ReadMetadataTest.php
@@ -6,6 +6,9 @@ namespace Flow\ParquetViewer\Tests\Integration;
 
 use Flow\ParquetViewer\Parquet;
 use PHPUnit\Framework\TestCase;
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Output\BufferedOutput;
+use Symfony\Component\Console\Style\{OutputStyle, SymfonyStyle};
 use Symfony\Component\Console\Tester\ApplicationTester;
 
 final class ReadMetadataTest extends TestCase
@@ -24,10 +27,11 @@ final class ReadMetadataTest extends TestCase
             'file' => $path,
         ]);
 
-        self::assertStringContainsString(
-            'not a valid parquet file',
-            $tester->getDisplay()
+        $expected = $this->captureConsoleOutput(
+            fn (OutputStyle $io) => $io->error("File \"{$path}\" is not a valid parquet file")
         );
+
+        self::assertStringContainsString($expected, $tester->getDisplay());
         self::assertSame(1, $tester->getStatusCode());
     }
 
@@ -55,5 +59,16 @@ final class ReadMetadataTest extends TestCase
         self::assertStringContainsString('Column Chunks Statistics', $tester->getDisplay());
         self::assertStringContainsString('Page Headers', $tester->getDisplay());
         self::assertSame(0, $tester->getStatusCode());
+    }
+
+    private function captureConsoleOutput(\Closure $closure) : string
+    {
+        $output = new BufferedOutput();
+
+        $io = new SymfonyStyle(new ArrayInput([]), $output);
+
+        $closure($io);
+
+        return $output->fetch();
     }
 }
