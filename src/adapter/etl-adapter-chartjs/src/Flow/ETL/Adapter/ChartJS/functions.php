0ac066d79117761f279ac1868ab399bb8de73ef2
Norbert Orzechowicz <contact@norbert.tech>
2024-08-16 00:06:43 +0200

    Added missing provider

--- src/adapter/etl-adapter-chartjs/src/Flow/ETL/Adapter/ChartJS/functions.php
+++ src/adapter/etl-adapter-chartjs/src/Flow/ETL/Adapter/ChartJS/functions.php
@@ -6,23 +6,30 @@ namespace Flow\ETL\Adapter\ChartJS;
 
 use Flow\ETL\Adapter\ChartJS\Chart\{BarChart, LineChart, PieChart};
 use Flow\ETL\Row\{EntryReference, References};
+use Flow\ETL\Attribute\DSL;
+use Flow\ETL\Attribute\Module;
+use Flow\ETL\Attribute\Type;
 use Flow\Filesystem\Path;
 
+#[DSL(module: Module::CHARTJS, type: Type::HELPER)]
 function bar_chart(EntryReference $label, References $datasets) : BarChart
 {
     return new BarChart($label, $datasets);
 }
 
+#[DSL(module: Module::CHARTJS, type: Type::HELPER)]
 function line_chart(EntryReference $label, References $datasets) : LineChart
 {
     return new LineChart($label, $datasets);
 }
 
+#[DSL(module: Module::CHARTJS, type: Type::HELPER)]
 function pie_chart(EntryReference $label, References $datasets) : PieChart
 {
     return new PieChart($label, $datasets);
 }
 
+#[DSL(module: Module::CHARTJS, type: Type::LOADER)]
 function to_chartjs_file(Chart $type, Path|string|null $output = null, Path|string|null $template = null) : ChartJSLoader
 {
     if (\is_string($output)) {
@@ -40,6 +47,7 @@ function to_chartjs_file(Chart $type, Path|string|null $output = null, Path|stri
     return new ChartJSLoader($type, output: $output, template: $template);
 }
 
+#[DSL(module: Module::CHARTJS, type: Type::LOADER)]
 function to_chartjs_var(Chart $type, array &$output) : ChartJSLoader
 {
     /** @psalm-suppress ReferenceConstraintViolation */
--- web/landing/src/Flow/Website/StaticSourceProvider/DSLProvider.php
+++ web/landing/src/Flow/Website/StaticSourceProvider/DSLProvider.php
@@ -22,6 +22,13 @@ final class DSLProvider implements SourceProvider
             $sources[] = new Source('documentation_dsl', ['module' => (new Slugify())->slugify($module)]);
         }
 
+        foreach ($this->dslDefinitions->all() as $definition) {
+            if ($definition->module() === null) {
+                dd($definition);
+            }
+            $sources[] = new Source('documentation_dsl_function', ['module' => (new Slugify())->slugify($definition->module()), 'function' => (new Slugify())->slugify($definition->name())]);
+        }
+
         return $sources;
     }
 }
