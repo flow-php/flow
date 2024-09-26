363234da430e868a205d237843b1667014d308dc
Norbert Orzechowicz <contact@norbert.tech>
2024-09-26 22:18:43 +0200

    Ignore first failure when removing container

--- src/lib/azure-sdk/src/Flow/Azure/SDK/Exception/AzureException.php
+++ src/lib/azure-sdk/src/Flow/Azure/SDK/Exception/AzureException.php
@@ -10,6 +10,6 @@ final class AzureException extends Exception
 {
     public function __construct(string $endpoint, public readonly RequestInterface $request, public readonly ResponseInterface $response)
     {
-        parent::__construct(\sprintf('Azure SDK Exception: %s', $endpoint));
+        parent::__construct(\sprintf('Azure SDK Exception: %s, %d, %s', $endpoint, $this->response->getStatusCode(), $this->response->getBody()->getContents()));
     }
 }
