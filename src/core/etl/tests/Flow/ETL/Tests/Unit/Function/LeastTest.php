a1181a77deda8aa71d503592b4595dd6b965a11f
Norbert Orzechowicz <contact@norbert.tech>
2025-01-02 19:14:03 -0600

    Fixed failing tests

--- src/core/etl/tests/Flow/ETL/Tests/Unit/Function/LeastTest.php
+++ src/core/etl/tests/Flow/ETL/Tests/Unit/Function/LeastTest.php
@@ -11,36 +11,36 @@ final class LeastTest extends FlowTestCase
 {
     public function test_greatest_with_non_comparable_values() : void
     {
-        $greatest = least([
+        $lest = least(
             null,
             20,
             ref('int'),
             new \DateTimeImmutable('now'),
-        ]);
+        );
 
-        $this->expectExceptionMessage("Can't compare '(datetime > integer)' due to data type mismatch.");
+        $this->expectExceptionMessage("Can't compare '(datetime < integer)' due to data type mismatch.");
 
-        $greatest->eval(row(int_entry('int', 55)));
+        $lest->eval(row(int_entry('int', 55)));
     }
 
     public function test_least_value() : void
     {
-        $greatest = least([
+        $lest = least(
             10,
             20,
             ref('int'),
             40,
-        ]);
+        );
 
         self::assertSame(
             10,
-            $greatest->eval(row(int_entry('int', 55)))
+            $lest->eval(row(int_entry('int', 55)))
         );
     }
 
     public function test_least_with_null() : void
     {
-        $greatest = least(
+        $ltest = least(
             null,
             20,
             ref('int'),
@@ -48,7 +48,7 @@ final class LeastTest extends FlowTestCase
         );
 
         self::assertNull(
-            $greatest->eval(row(int_entry('int', 4)))
+            $ltest->eval(row(int_entry('int', 4)))
         );
     }
 }
