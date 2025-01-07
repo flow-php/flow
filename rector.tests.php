59e2564ff1a56432fd7a41cbb91cd9e4e14ce896
Norbert Orzechowicz <contact@norbert.tech>
2025-01-07 10:26:31 -0600

    Fixed importing names

--- rector.tests.php
+++ rector.tests.php
@@ -2,7 +2,46 @@
 
 use Flow\ETL\Config;
 use Flow\ETL\FlowContext;
+use Flow\ETL\PHP\Type\Logical\DateTimeType;
+use Flow\ETL\PHP\Type\Logical\DateType;
+use Flow\ETL\PHP\Type\Logical\JsonType;
+use Flow\ETL\PHP\Type\Logical\List\ListElement;
+use Flow\ETL\PHP\Type\Logical\ListType;
+use Flow\ETL\PHP\Type\Logical\Map\MapKey;
+use Flow\ETL\PHP\Type\Logical\MapType;
+use Flow\ETL\PHP\Type\Logical\Structure\StructureElement;
+use Flow\ETL\PHP\Type\Logical\StructureType;
+use Flow\ETL\PHP\Type\Logical\TimeType;
+use Flow\ETL\PHP\Type\Logical\UuidType;
+use Flow\ETL\PHP\Type\Logical\XMLElementType;
+use Flow\ETL\PHP\Type\Logical\XMLType;
+use Flow\ETL\PHP\Type\Native\ArrayType;
+use Flow\ETL\PHP\Type\Native\BooleanType;
+use Flow\ETL\PHP\Type\Native\CallableType;
+use Flow\ETL\PHP\Type\Native\EnumType;
+use Flow\ETL\PHP\Type\Native\FloatType;
+use Flow\ETL\PHP\Type\Native\IntegerType;
+use Flow\ETL\PHP\Type\Native\NullType;
+use Flow\ETL\PHP\Type\Native\ObjectType;
+use Flow\ETL\PHP\Type\Native\ResourceType;
+use Flow\ETL\PHP\Type\Native\StringType;
 use Flow\ETL\Row;
+use Flow\ETL\Row\Entry\BooleanEntry;
+use Flow\ETL\Row\Entry\DateEntry;
+use Flow\ETL\Row\Entry\DatetimeEntry;
+use Flow\ETL\Row\Entry\EnumEntry;
+use Flow\ETL\Row\Entry\FloatEntry;
+use Flow\ETL\Row\Entry\IntegerEntry;
+use Flow\ETL\Row\Entry\JsonEntry;
+use Flow\ETL\Row\Entry\ListEntry;
+use Flow\ETL\Row\Entry\MapEntry;
+use Flow\ETL\Row\Entry\StringEntry;
+use Flow\ETL\Row\Entry\StructureEntry;
+use Flow\ETL\Row\Entry\TimeEntry;
+use Flow\ETL\Row\Entry\UuidEntry;
+use Flow\ETL\Row\Entry\XMLElementEntry;
+use Flow\ETL\Row\Entry\XMLEntry;
+use Flow\ETL\Row\Schema\Definition;
 use Flow\ETL\Rows;
 use Flow\Tools\Rector\NewObjectToFunction;
 use Flow\Tools\Rector\NewToFunctionCallRector;
@@ -26,18 +65,102 @@ return RectorConfig::configure()
     ->withConfiguredRule(
         StaticCallToFuncCallRector::class,
         [
+            // Building Blocks
             new StaticCallToFuncCall(Flow\ETL\Row::class, 'create', 'Flow\ETL\DSL\row'),
-            new StaticCallToFuncCall(Config::class, 'default', 'Flow\ETL\DSL\config')
+            new StaticCallToFuncCall(Config::class, 'default', 'Flow\ETL\DSL\config'),
+            // Schema
+            new StaticCallToFuncCall(Definition::class, 'boolean', 'Flow\ETL\DSL\bool_schema'),
+            new StaticCallToFuncCall(Definition::class, 'date', 'Flow\ETL\DSL\date_schema'),
+            new StaticCallToFuncCall(Definition::class, 'datetime', 'Flow\ETL\DSL\datetime_schema'),
+            new StaticCallToFuncCall(Definition::class, 'enum', 'Flow\ETL\DSL\enum_schema'),
+            new StaticCallToFuncCall(Definition::class, 'float', 'Flow\ETL\DSL\float_schema'),
+            new StaticCallToFuncCall(Definition::class, 'integer', 'Flow\ETL\DSL\integer_schema'),
+            new StaticCallToFuncCall(Definition::class, 'json', 'Flow\ETL\DSL\json_schema'),
+            new StaticCallToFuncCall(Definition::class, 'list', 'Flow\ETL\DSL\list_schema'),
+            new StaticCallToFuncCall(Definition::class, 'map', 'Flow\ETL\DSL\map_schema'),
+            new StaticCallToFuncCall(Definition::class, 'string', 'Flow\ETL\DSL\string_schema'),
+            new StaticCallToFuncCall(Definition::class, 'structure', 'Flow\ETL\DSL\structure_schema'),
+            new StaticCallToFuncCall(Definition::class, 'time', 'Flow\ETL\DSL\time_schema'),
+            new StaticCallToFuncCall(Definition::class, 'uuid', 'Flow\ETL\DSL\uuid_schema'),
+            new StaticCallToFuncCall(Definition::class, 'xml', 'Flow\ETL\DSL\xml_schema'),
+            new StaticCallToFuncCall(Definition::class, 'xml_element', 'Flow\ETL\DSL\xml_element_schema'),
+            // Logical Types
+            new StaticCallToFuncCall(MapKey::class, 'integer', 'Flow\ETL\DSL\type_integer'),
+            new StaticCallToFuncCall(MapKey::class, 'string', 'Flow\ETL\DSL\type_string'),
+
+            new StaticCallToFuncCall(ListElement::class, 'boolean', 'Flow\ETL\DSL\type_boolean'),
+            new StaticCallToFuncCall(ListElement::class, 'datetime', 'Flow\ETL\DSL\type_datetime'),
+            new StaticCallToFuncCall(ListElement::class, 'float', 'Flow\ETL\DSL\type_float'),
+            new StaticCallToFuncCall(ListElement::class, 'integer', 'Flow\ETL\DSL\type_integer'),
+            new StaticCallToFuncCall(ListElement::class, 'json', 'Flow\ETL\DSL\type_json'),
+            new StaticCallToFuncCall(ListElement::class, 'list', 'Flow\ETL\DSL\type_list'),
+            new StaticCallToFuncCall(ListElement::class, 'map', 'Flow\ETL\DSL\type_map'),
+            new StaticCallToFuncCall(ListElement::class, 'object', 'Flow\ETL\DSL\type_object'),
+            new StaticCallToFuncCall(ListElement::class, 'string', 'Flow\ETL\DSL\type_string'),
+            new StaticCallToFuncCall(ListElement::class, 'structure', 'Flow\ETL\DSL\type_structure'),
+            new StaticCallToFuncCall(ListElement::class, 'uuid', 'Flow\ETL\DSL\type_uuid'),
+            new StaticCallToFuncCall(ListElement::class, 'xml', 'Flow\ETL\DSL\type_xml'),
+            new StaticCallToFuncCall(ListElement::class, 'xml_element', 'Flow\ETL\DSL\type_xml_element'),
         ]
     )
     ->withConfiguredRule(
         NewToFunctionCallRector::class,
         [
+            // Building Blocks
             new NewObjectToFunction(Rows::class, 'Flow\ETL\DSL\rows'),
             new NewObjectToFunction(Config::class, 'Flow\ETL\DSL\config'),
             new NewObjectToFunction(FlowContext::class, 'Flow\ETL\DSL\flow_context'),
+            new NewObjectToFunction(Flow\ETL\Row\Schema::class, 'Flow\ETL\DSL\schema'),
+
+            // Entries
+            new NewObjectToFunction(BooleanEntry::class, 'Flow\ETL\DSL\boolean_entry'),
+            new NewObjectToFunction(DateEntry::class, 'Flow\ETL\DSL\date_entry'),
+            new NewObjectToFunction(DatetimeEntry::class, 'Flow\ETL\DSL\datetime_entry'),
+            new NewObjectToFunction(EnumEntry::class, 'Flow\ETL\DSL\enum_entry'),
+            new NewObjectToFunction(FloatEntry::class, 'Flow\ETL\DSL\float_entry'),
+            new NewObjectToFunction(IntegerEntry::class, 'Flow\ETL\DSL\integer_entry'),
+            new NewObjectToFunction(JsonEntry::class, 'Flow\ETL\DSL\json_entry'),
+            new NewObjectToFunction(ListEntry::class, 'Flow\ETL\DSL\list_entry'),
+            new NewObjectToFunction(MapEntry::class, 'Flow\ETL\DSL\map_entry'),
+            new NewObjectToFunction(StringEntry::class, 'Flow\ETL\DSL\string_entry'),
+            new NewObjectToFunction(StructureEntry::class, 'Flow\ETL\DSL\structure_entry'),
+            new NewObjectToFunction(TimeEntry::class, 'Flow\ETL\DSL\time_entry'),
+            new NewObjectToFunction(UuidEntry::class, 'Flow\ETL\DSL\uuid_entry'),
+            new NewObjectToFunction(XMLElementEntry::class, 'Flow\ETL\DSL\xml_element_entry'),
+            new NewObjectToFunction(XMLEntry::class, 'Flow\ETL\DSL\xml_entry'),
+
+            // Native Types
+            new NewObjectToFunction(ArrayType::class, 'Flow\ETL\DSL\type_array'),
+            new NewObjectToFunction(BooleanType::class, 'Flow\ETL\DSL\type_boolean'),
+            new NewObjectToFunction(CallableType::class, 'Flow\ETL\DSL\type_callable'),
+            new NewObjectToFunction(EnumType::class, 'Flow\ETL\DSL\type_enum'),
+            new NewObjectToFunction(FloatType::class, 'Flow\ETL\DSL\type_float'),
+            new NewObjectToFunction(IntegerType::class, 'Flow\ETL\DSL\type_integer'),
+            new NewObjectToFunction(NullType::class, 'Flow\ETL\DSL\type_null'),
+            new NewObjectToFunction(ObjectType::class, 'Flow\ETL\DSL\type_object'),
+            new NewObjectToFunction(ResourceType::class, 'Flow\ETL\DSL\type_resource'),
+            new NewObjectToFunction(StringType::class, 'Flow\ETL\DSL\type_string'),
+
+            // Logical Types
+            new NewObjectToFunction(DateTimeType::class, 'Flow\ETL\DSL\type_datetime'),
+            new NewObjectToFunction(DateType::class, 'Flow\ETL\DSL\type_date'),
+            new NewObjectToFunction(JsonType::class, 'Flow\ETL\DSL\type_json'),
+            new NewObjectToFunction(ListType::class, 'Flow\ETL\DSL\type_list'),
+            new NewObjectToFunction(MapType::class, 'Flow\ETL\DSL\type_map'),
+            new NewObjectToFunction(StructureType::class, 'Flow\ETL\DSL\type_structure'),
+            new NewObjectToFunction(TimeType::class, 'Flow\ETL\DSL\type_time'),
+            new NewObjectToFunction(UuidType::class, 'Flow\ETL\DSL\type_uuid'),
+            new NewObjectToFunction(XMLElementType::class, 'Flow\ETL\DSL\type_xml_element'),
+            new NewObjectToFunction(XMLType::class, 'Flow\ETL\DSL\type_xml'),
+            new NewObjectToFunction(StructureElement::class, 'Flow\ETL\DSL\structure_element'),
         ]
     )
     ->withSkip([
         RemoveParentCallWithoutParentRector::class
-    ]);
+    ])
+    ->withImportNames(
+        importNames: true,
+        importDocBlockNames: true,
+        importShortClasses: false,
+        removeUnusedImports: true
+    );
