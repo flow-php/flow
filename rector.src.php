a149b3c0e1e57b1456f4ee206b1a1aabcacc0762
Norbert Orzechowicz <contact@norbert.tech>
2025-01-06 22:32:45 -0600

    Configure rector for src and tests

---
+++ rector.src.php
@@ -0,0 +1,18 @@
+<?php
+
+use Rector\Config\RectorConfig;
+use Rector\Set\ValueObject\LevelSetList;
+
+return RectorConfig::configure()
+    ->withPaths([
+        __DIR__ . '/bin',
+        __DIR__ . '/examples',
+        __DIR__ . '/src/core/etl/src',
+        __DIR__ . '/src/cli/src',
+        __DIR__ . '/src/adapter/*/src',
+        __DIR__ . '/src/bridge/*/*/src',
+        __DIR__ . '/src/tools/*/*/src',
+    ])
+    ->withSets([
+        LevelSetList::UP_TO_PHP_82
+    ]);
---
+++ rector.tests.php
@@ -0,0 +1,16 @@
+<?php
+
+use Rector\Config\RectorConfig;
+use Rector\Set\ValueObject\LevelSetList;
+
+return RectorConfig::configure()
+    ->withPaths([
+        __DIR__ . '/src/core/etl/tests',
+        __DIR__ . '/src/cli/tests',
+        __DIR__ . '/src/adapter/*/tests',
+        __DIR__ . '/src/bridge/*/*/tests',
+        __DIR__ . '/src/tools/*/*/tests',
+    ])
+    ->withSets([
+        LevelSetList::UP_TO_PHP_82
+    ]);
