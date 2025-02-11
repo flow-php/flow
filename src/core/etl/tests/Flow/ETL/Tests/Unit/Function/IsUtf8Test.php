0ddb0a9883159d218462dc1e18f3d863860df212
f-lapinski <filip.lapinski@proton.me>
2025-02-11 10:46:56 +0100

    Add Test for Return Type of IsUtf8()

--- src/core/etl/tests/Flow/ETL/Tests/Unit/Function/IsUtf8Test.php
+++ src/core/etl/tests/Flow/ETL/Tests/Unit/Function/IsUtf8Test.php
@@ -5,7 +5,9 @@ declare(strict_types=1);
 namespace Flow\ETL\Tests\Unit\Function;
 
 use function Flow\ETL\DSL\row;
-use function Flow\ETL\DSL\{ref, str_entry};
+use function Flow\ETL\DSL\{ref, str_entry, type_boolean};
+use Flow\ETL\Function\IsUtf8;
+use Flow\ETL\PHP\Type\Type;
 use Flow\ETL\Tests\FlowTestCase;
 
 final class IsUtf8Test extends FlowTestCase
@@ -23,5 +25,15 @@ final class IsUtf8Test extends FlowTestCase
                 row(str_entry('str', "\xc3\x28"))
             )
         );
+    }
+
+    public function test_returns_method_returns_string_boolean() : void
+    {
+        $isUtf8Function = new IsUtf8('Lorem Ipsum');
+        $returnType = $isUtf8Function->returns();
+
+        self::assertInstanceOf(Type::class, $returnType);
+
+        self::assertTrue($returnType->isEqual(type_boolean()));
     }
 }
