36a531e2e9aabda13bbbb0aeb7a874bb411ed5d9
f-lapinski <filip.lapinski@proton.me>
2025-02-14 12:00:30 +0100

    Fix for Symfony < 7.1 and replace localeTitle() for normal title() when method localTitle() doesn't exist

--- src/core/etl/src/Flow/ETL/Function/StringLocaleTitle.php
+++ src/core/etl/src/Flow/ETL/Function/StringLocaleTitle.php
@@ -9,6 +9,7 @@ use function Symfony\Component\String\u;
 use Flow\ETL\Function\ScalarFunction\TypedScalarFunction;
 use Flow\ETL\PHP\Type\Type;
 use Flow\ETL\Row;
+use Symfony\Component\String\AbstractUnicodeString;
 
 final class StringLocaleTitle extends ScalarFunctionChain implements TypedScalarFunction
 {
@@ -25,6 +26,11 @@ final class StringLocaleTitle extends ScalarFunctionChain implements TypedScalar
 
         if ($string === null || $locale === null) {
             return null;
+        }
+
+        /** @phpstan-ignore-next-line */
+        if (!method_exists(AbstractUnicodeString::class, 'localeTitle')) {
+            return u($string)->title()->toString();
         }
 
         return u($string)->localeTitle($locale)->toString();
--- src/core/etl/tests/Flow/ETL/Tests/Unit/Function/StringLocaleTitleTest.php
+++ src/core/etl/tests/Flow/ETL/Tests/Unit/Function/StringLocaleTitleTest.php
@@ -9,6 +9,7 @@ use function Flow\ETL\DSL\{ref, str_entry, type_string};
 use Flow\ETL\Function\StringLocaleTitle;
 use Flow\ETL\PHP\Type\Type;
 use Flow\ETL\Tests\FlowTestCase;
+use Symfony\Component\String\AbstractUnicodeString;
 
 final class StringLocaleTitleTest extends FlowTestCase
 {
@@ -24,21 +25,42 @@ final class StringLocaleTitleTest extends FlowTestCase
 
     public function test_string_locale_title_en() : void
     {
-        self::assertSame(
-            'Foo ijssel',
-            ref('str')->stringLocaleTitle('en')->eval(
-                row(str_entry('str', 'foo ijssel'))
-            )
-        );
+        /** @phpstan-ignore-next-line */
+        if (!method_exists(AbstractUnicodeString::class, 'localeTitle')) {
+            self::assertSame(
+                'Foo ijssel',
+                ref('str')->stringLocaleTitle('en')->eval(
+                    row(str_entry('str', 'foo ijssel'))
+                )
+            );
+        } else {
+            self::assertSame(
+                'Foo ijssel',
+                ref('str')->stringTitle()->eval(
+                    row(str_entry('str', 'foo ijssel'))
+                )
+            );
+        }
     }
 
     public function test_string_locale_title_nl() : void
     {
-        self::assertSame(
-            'Foo IJssel',
-            ref('str')->stringLocaleTitle('nl')->eval(
-                row(str_entry('str', 'foo ijssel'))
-            )
-        );
+        /** @phpstan-ignore-next-line */
+        if (!method_exists(AbstractUnicodeString::class, 'localeTitle')) {
+            self::assertSame(
+                'Foo IJssel',
+                ref('str')->stringLocaleTitle('nl')->eval(
+                    row(str_entry('str', 'foo ijssel'))
+                )
+            );
+        } else {
+            self::assertNotSame(
+                'Foo IJssel',
+                ref('str')->stringTitle()->eval(
+                    row(str_entry('str', 'foo ijssel'))
+                )
+            );
+        }
+
     }
 }
