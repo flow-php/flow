18578e20b1e6699bf42f6bf449ee7df8bf70dac2
Norbert Orzechowicz <contact@norbert.tech>
2025-04-10 11:32:13 +0200

    fixed order of conditions in checking if cache exists

--- src/core/etl/src/Flow/ETL/Config/Cache/CacheConfigBuilder.php
+++ src/core/etl/src/Flow/ETL/Config/Cache/CacheConfigBuilder.php
@@ -26,7 +26,7 @@ final class CacheConfigBuilder
         $cachePath = $cachePath !== '' ? $cachePath : \sys_get_temp_dir() . '/flow_php/cache';
 
         if (!is_dir($cachePath)) {
-            if (false === @mkdir($cachePath, 0777, true) && !is_dir($cachePath)) {
+            if (true === @mkdir($cachePath, 0777, true) || is_dir($cachePath)) {
                 // Directory either was created or already exists, proceed
             } else {
                 throw new RuntimeException(sprintf('Can\'t create cache directory: "%s" Please use a different one through %s environment variable', $cachePath, CacheConfig::CACHE_DIR_ENV));
