93f82a2339f885143800ab008bebecc9bc5404ac
Joseph Bielawski <stloyd@gmail.com>
2025-05-04 09:23:21 +0200

    Add a new `DataFrame::renameEach()`

--- web/landing/tests/Flow/Website/Tests/Functional/DocumentationTest.php
+++ web/landing/tests/Flow/Website/Tests/Functional/DocumentationTest.php
@@ -33,7 +33,7 @@ final class DocumentationTest extends WebTestCase
         self::assertGreaterThan(0, $client->getCrawler()->filter('[data-dsl-function]')->count());
         self::assertGreaterThan(0, $client->getCrawler()->filter('[data-dsl-source-link]')->count());
         self::assertEquals(16, $client->getCrawler()->filter('[data-dsl-module]')->count());
-        self::assertEquals(11, $client->getCrawler()->filter('[data-dsl-type]')->count());
+        self::assertEquals(12, $client->getCrawler()->filter('[data-dsl-type]')->count());
     }
 
     protected static function getKernelClass() : string
