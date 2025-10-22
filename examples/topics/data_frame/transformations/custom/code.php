789380fa355e1d201909bf1d100ec9fed268c6af
Norbert Orzechowicz <contact@norbert.tech>
2025-10-22 15:01:56 +0200

    Added examples of creating reusable Transformations, make composer.json section collapsed by default

---
+++ examples/topics/data_frame/transformations/custom/code.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+use function Flow\ETL\DSL\{concat_ws, data_frame, from_array, lit, ref, to_stream};
+use Flow\ETL\Transformation;
+
+require __DIR__ . '/vendor/autoload.php';
+
+data_frame()
+    ->read(
+        from_array([
+            ['id' => 1, 'first_name' => 'John', 'last_name' => 'Doe'],
+            ['id' => 2, 'first_name' => 'Jane', 'last_name' => 'Smith'],
+            ['id' => 3, 'first_name' => 'Bob', 'last_name' => 'Johnson'],
+            ['id' => 4, 'first_name' => 'Alice', 'last_name' => 'Williams'],
+        ])
+    )
+    ->with(
+        /**
+         *  Create new column "name" by concatenating "first_name" and "last_name" columns.
+         */
+        new class implements Transformation {
+            public function transform(Flow\ETL\DataFrame $dataFrame) : Flow\ETL\DataFrame
+            {
+                return $dataFrame->withEntry('name', concat_ws(lit(' '), ref('first_name'), ref('last_name')));
+            }
+        }
+    )
+    ->collect()
+    ->write(to_stream(__DIR__ . '/output.txt', truncate: false))
+    ->run();
