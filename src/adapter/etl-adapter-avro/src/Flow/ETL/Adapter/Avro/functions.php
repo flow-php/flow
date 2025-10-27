72208ec459d04161fd26e56b72fd5fec086bbe33
Joseph Bielawski <stloyd@gmail.com>
2025-10-27 17:40:12 +0100

    Remove multiple path support in `from_avro()` function Don't validate it due to lack of support

--- src/adapter/etl-adapter-avro/src/Flow/ETL/Adapter/Avro/functions.php
+++ src/adapter/etl-adapter-avro/src/Flow/ETL/Adapter/Avro/functions.php
@@ -4,27 +4,13 @@ declare(strict_types=1);
 
 namespace Flow\ETL\DSL\Adapter\Avro;
 
-use function Flow\ETL\DSL\from_all;
 use Flow\ETL\Adapter\Avro\FlixTech\{AvroExtractor, AvroLoader};
-use Flow\ETL\{Attribute\DocumentationDSL, Attribute\Module, Attribute\Type, Extractor, Schema};
+use Flow\ETL\{Attribute\DocumentationDSL, Attribute\Module, Attribute\Type, Schema};
 use Flow\Filesystem\Path;
 
 #[DocumentationDSL(module: Module::AVRO, type: Type::EXTRACTOR)]
-function from_avro(Path|string|array $path) : Extractor
+function from_avro(Path|string $path) : AvroExtractor
 {
-    if (\is_array($path)) {
-        /** @var array<Extractor> $extractors */
-        $extractors = [];
-
-        foreach ($path as $next_path) {
-            $extractors[] = new AvroExtractor(
-                \is_string($next_path) ? Path::realpath($next_path) : $next_path,
-            );
-        }
-
-        return from_all(...$extractors);
-    }
-
     return new AvroExtractor(
         \is_string($path) ? Path::realpath($path) : $path
     );
