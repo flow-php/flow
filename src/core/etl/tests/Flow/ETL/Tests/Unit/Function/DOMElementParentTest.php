f883305a246edf13fab4088634ea79d1e4904d21
Joseph Bielawski <stloyd@gmail.com>
2025-11-10 19:40:42 +0100

    Add a new `DOMElementParent` function

--- src/core/etl/tests/Flow/ETL/Tests/Unit/Function/DOMElementParentTest.php
+++ src/core/etl/tests/Flow/ETL/Tests/Unit/Function/DOMElementParentTest.php
@@ -6,11 +6,27 @@ namespace Flow\ETL\Tests\Unit\Function;
 
 use function Flow\ETL\DSL\{config, flow_context, ref, row};
 use Dom\HTMLDocument;
+use Flow\ETL\Exception\InvalidArgumentException;
+use Flow\ETL\Function\ExecutionMode;
 use PHPUnit\Framework\Attributes\RequiresPhp;
 use PHPUnit\Framework\TestCase;
 
 final class DOMElementParentTest extends TestCase
 {
+    #[RequiresPhp('>= 8.4')]
+    public function test_html_fails_when_parent_not_available_in_strict_mode() : void
+    {
+        $element = HTMLDocument::createFromString('<span>bar</span>', \LIBXML_HTML_NOIMPLIED | \LIBXML_NOERROR);
+
+        $context = flow_context(config());
+        $context->functions()->setMode(ExecutionMode::STRICT);
+
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('DOMElementParent requires non-null DOMNode or HTMLElement.');
+
+        ref('value')->domElementParent()->eval(row($context->entryFactory()->create('value', $element->documentElement->parentElement)), $context);
+    }
+
     #[RequiresPhp('>= 8.4')]
     public function test_html_getting_parent_element() : void
     {
@@ -29,6 +45,23 @@ final class DOMElementParentTest extends TestCase
 
         self::assertNull(
             ref('value')->domElementParent()->eval(row(flow_context(config())->entryFactory()->create('value', $element->documentElement)), flow_context())
+        );
+    }
+
+    public function test_xml_fails_when_parent_not_available_in_strict_mode() : void
+    {
+        $xml = new \DOMDocument();
+        $xml->loadXML('<root>foobar</root>');
+
+        $context = flow_context(config());
+        $context->functions()->setMode(ExecutionMode::STRICT);
+
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('DOMElementParent requires non-null DOMNode or HTMLElement.');
+
+        self::assertEquals(
+            $xml,
+            ref('value')->domElementParent()->eval(row($context->entryFactory()->create('value', $xml->parentNode)), $context)
         );
     }
 
@@ -52,6 +85,17 @@ final class DOMElementParentTest extends TestCase
         self::assertEquals(
             $xml,
             ref('value')->domElementParent()->eval(row(flow_context(config())->entryFactory()->create('value', $xml->documentElement)), flow_context())
+        );
+    }
+
+    public function test_xml_getting_parent_element_when_passing_document() : void
+    {
+        $xml = new \DOMDocument();
+        $xml->loadXML('<root>foobar</root>');
+
+        self::assertEquals(
+            $xml,
+            ref('value')->domElementParent()->eval(row(flow_context(config())->entryFactory()->create('value', $xml)), flow_context())
         );
     }
 }
