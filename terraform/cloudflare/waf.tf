4bf0c6e5c2fcfb0e0f401691d1c3175ff04d1978
Norbert Orzechowicz <contact@norbert.tech>
2025-11-13 11:07:47 +0100

    feature: added geo restricting rules to playground

--- terraform/cloudflare/waf.tf
+++ terraform/cloudflare/waf.tf
@@ -7,9 +7,21 @@ resource "cloudflare_ruleset" "playground_bot_protection" {
 
   rules = [
     {
+      action      = "block"
+      expression  = "(http.request.uri.path contains \"/playground\" and ip.geoip.country in {\"CN\" \"RU\" \"VN\" \"IN\" \"BR\" \"ID\"})"
+      description = "Block playground access from CN, RU, VN, IN, BR, ID"
+      enabled     = true
+    },
+    {
       action      = "managed_challenge"
       expression  = "(http.request.uri.path contains \"/playground\")"
       description = "Managed challenge for playground access"
+      enabled     = true
+    },
+    {
+      action      = "challenge"
+      expression  = "(http.request.uri.path contains \"/playground\" and cf.threat_score gt 14)"
+      description = "Challenge IPs with bad reputation accessing playground"
       enabled     = true
     }
   ]
