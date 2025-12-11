f094d0aa50f677cd0e6d51bf840480be5538e46f
Norbert Orzechowicz <contact@norbert.tech>
2025-12-12 00:47:32 +0100

    chore: updated nix shell
    - drop 8.2 from nix shell
    - added 8.5 from nixpkgs PR
    - updated php extension in nix pkgs
    - added composer build:docker script
    - added missing extensions to Dockerfile installation

--- .nix/pkgs/php-snappy/package.nix
+++ .nix/pkgs/php-snappy/package.nix
@@ -2,12 +2,12 @@
 
 php.buildPecl {
   pname = "snappy";
-  version = "0.2.1";
+  version = "0.2.3";
 
   src = fetchFromGitHub {
     owner = "kjdev";
     repo = "php-ext-snappy";
-    tag = "0.2.1";
+    tag = "0.2.3";
     hash = "sha256-PAKdIcpJKH6d74EulYQepP4XbQvccrj1nEuir47vro4=";
     fetchSubmodules = true;
   };
--- .nix/pkgs/php-zstd/package.nix
+++ .nix/pkgs/php-zstd/package.nix
@@ -2,13 +2,13 @@
 
 php.buildPecl {
   pname = "zstd";
-  version = "0.14.0";
+  version = "0.15.2";
 
   src = fetchFromGitHub {
     owner = "kjdev";
     repo = "php-ext-zstd";
-    tag = "0.14.0";
-    hash = "sha256-oIbvaLYQ6Tp20Y/UEN7i1dtMnxGdMNcIjv6xRCyVYdE=";
+    tag = "0.15.2";
+    hash = "sha256-AhEEHtETALgYkJxDfRA6/bx6yZKpsfL48MqO505FAFI=";
     fetchSubmodules = true;
   };
 }
