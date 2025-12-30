6cd8ae0cdf97df48cf11a1dae6b1d96d3fcdf1dd
Joseph Bielawski <stloyd@gmail.com>
2025-12-30 17:26:06 +0100

    chore: Remove old OpenSpout code fallback

--- src/adapter/etl-adapter-excel/src/Flow/ETL/Adapter/Excel/ExcelExtractor.php
+++ src/adapter/etl-adapter-excel/src/Flow/ETL/Adapter/Excel/ExcelExtractor.php
@@ -133,8 +133,7 @@ final class ExcelExtractor implements Extractor, FileExtractor, LimitableExtract
         $rowData = \array_map(
             // Convert empty values to nullables if allowed
             fn (Cell $cell) => $this->convertEmptyToNull && '' === $cell->getValue() ? null : $cell->getValue(),
-            /** @phpstan-ignore-next-line method remove in 5.0 in favor of public property */
-            method_exists($row, 'getCells') ? $row->getCells() : $row->cells
+            $row->cells
         );
 
         // Expand columns to the size of the previous row
