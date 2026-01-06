08ddfc8996e4ea81d0a81c6b58c09f1d943db7b7
Norbert Orzechowicz <1921950+norberttech@users.noreply.github.com>
2026-01-06 23:20:57 +0100

    Update Homebrew TAP formula: flow-php to version: 0.30.0
    <h2>Change Log</h2>
    <div id="change-log">
      <h4>Changed</h4>
      <ul id="changed">
        <li>Update Homebrew TAP formula: flow-php to version: 0.30.0</li>
      </ul>
    </div>
    <hr/>
    
    <h2>Description</h2>
    
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- src/tools/homebrew/Formula/flow-php.rb
+++ src/tools/homebrew/Formula/flow-php.rb
@@ -1,8 +1,8 @@
 class FlowPhp < Formula
   desc "Flow is a first and the most advanced PHP ETL framework"
   homepage "https://github.com/flow-php/flow"
-  url "https://github.com/flow-php/flow/releases/download/0.29.0/flow.phar"
-  sha256 "a147b18f560e7a60b24b24a712f2ae718cccddd1d6eaec65e6b091bb1e9156d7"
+  url "https://github.com/flow-php/flow/releases/download/0.30.0/flow.phar"
+  sha256 "79d7ad29ef6cb7f584a53ff29882bedccc61bb038aa7705567fe2ea51271bf8c"
   license "MIT"
 
   depends_on "php"
