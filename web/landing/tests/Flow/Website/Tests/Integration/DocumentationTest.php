b733f4c983d9023d3208765cb191b3108a4704d7
Norbert Orzechowicz <contact@norbert.tech>
2026-01-16 20:35:10 +0100

    fix: documentation tests on website

--- web/landing/tests/Flow/Website/Tests/Integration/DocumentationTest.php
+++ web/landing/tests/Flow/Website/Tests/Integration/DocumentationTest.php
@@ -32,7 +32,7 @@ final class DocumentationTest extends WebTestCase
         self::assertResponseIsSuccessful();
         self::assertGreaterThan(0, $client->getCrawler()->filter('[data-dsl-function]')->count());
         self::assertGreaterThan(0, $client->getCrawler()->filter('[data-dsl-source-link]')->count());
-        self::assertEquals(21, $client->getCrawler()->filter('[data-dsl-module]')->count());
+        self::assertGreaterThan(21, $client->getCrawler()->filter('[data-dsl-module]')->count());
         self::assertEquals(12, $client->getCrawler()->filter('[data-dsl-type]')->count());
     }
 
