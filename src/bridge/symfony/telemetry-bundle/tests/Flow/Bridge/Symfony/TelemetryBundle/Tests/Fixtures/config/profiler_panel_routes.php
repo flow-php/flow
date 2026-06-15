46eebfce3698020b2384260527182cf9c4d77521
Joseph Bielawski <stloyd@users.noreply.github.com>
2026-06-15 13:53:00 +0200

    chore: Fallback to the Symfony XML configuration only at `^6.4.0` (#2450)

--- src/bridge/symfony/telemetry-bundle/tests/Flow/Bridge/Symfony/TelemetryBundle/Tests/Fixtures/config/profiler_panel_routes.php
+++ src/bridge/symfony/telemetry-bundle/tests/Flow/Bridge/Symfony/TelemetryBundle/Tests/Fixtures/config/profiler_panel_routes.php
@@ -7,9 +7,9 @@ use Composer\Semver\VersionParser;
 use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
 
 return static function (RoutingConfigurator $routes): void {
-    if (InstalledVersions::satisfies(new VersionParser(), 'symfony/web-profiler-bundle', '^7.4')) {
-        $routes->import('@WebProfilerBundle/Resources/config/routing/profiler.php')->prefix('/_profiler');
-    } else {
+    if (InstalledVersions::satisfies(new VersionParser(), 'symfony/web-profiler-bundle', '^6.4.0')) {
         $routes->import('@WebProfilerBundle/Resources/config/routing/profiler.xml')->prefix('/_profiler');
+    } else {
+        $routes->import('@WebProfilerBundle/Resources/config/routing/profiler.php')->prefix('/_profiler');
     }
 };
